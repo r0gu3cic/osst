ff984ba46a4ed53d615971f20da294df1ae3aec9
Stefan Stosic <stosic.n.stefan@gmail.com>
2025-05-17 20:19:56 +0200

    improved blacklist logic for service_info

--- osst/services_info.py
+++ osst/services_info.py
@@ -5,7 +5,7 @@ def get_active_services():
     Returns a list of active (running) systemd services on the system.
     """
     # We blacklist some common services
-    blacklist = {
+    blacklist_exact = {
         'systemd-journald.service',
         'systemd-logind.service',
         'systemd-networkd.service',
@@ -17,7 +17,16 @@ def get_active_services():
         'polkit.service',
         'packagekit.service',
         'snapd.service',
+        'getty@tty1.service',
+        'irqbalance.service',
+        'ModemManager.service',
+        'multipathd.service',
+        'networkd-dispatcher.service',
+        'udisks2.service', 
     }
+    blacklist_regex = [
+        r'^user@\d+\.service$', # Ignore services that starts with user
+    ]
     try:
         # We call systemctl to get only active (running) services in short format
         result = subprocess.run(
@@ -32,8 +41,16 @@ def get_active_services():
             if line:
                 # The first column is the name of the service, e.g. ssh.service
                 service_name = line.split()[0]
-                if service_name not in blacklist:
-                    services.append(service_name)
+
+                # If it is in the exact blacklist
+                if service_name in blacklist_exact:
+                    continue
+
+                # If a regex matches
+                if any(re.match(pattern, service_name) for pattern in blacklist_regex):
+                    continue
+
+                services.append(service_name)
         return services
     except Exception as e:
         # In case of error, return an empty list or you can log the error
