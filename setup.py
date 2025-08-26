3da83832ccc95422e3b7ccbed375989b9b207e90
Stefan Stosic <stosic.n.stefan@gmail.com>
2025-08-26 16:58:02 +0200

    build dependancy hell 2, fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="osst",
     version="0.1.0",
     description="Operating System Story Teller (OSST)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="enabler",
     packages=find_packages(),
     entry_points={"console_scripts": ["osst=osst.__main__:main"]},
